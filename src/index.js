d4b9902e224496d539ff8e4d8e428e7cab1001cd
Diego Rocha <dkxever@gmail.com>
2022-01-28 19:53:05 -0300

    Create context and provider

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import SettingsContextProvider from './Context/SettingsContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SettingsContextProvider>
+      <App />
+    </SettingsContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
