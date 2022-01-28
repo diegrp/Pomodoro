26e3ae67c5d89a41278aa6f7fd41f59fc281cb26
Diego Rocha <dkxever@gmail.com>
2022-01-28 18:55:08 -0300

    Create required components for the app

---
+++ src/Components/CountdownAnimation/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CountdownAnimation = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default CountdownAnimation
---
+++ src/Components/SetPomodoro/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SetPomodoro = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default SetPomodoro
