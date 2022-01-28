d790cabe168de188a2f9ac9742c0a03e097f458d
Diego Rocha <dkxever@gmail.com>
2022-01-28 19:12:11 -0300

    Import and build the form in component SetPomodoro

--- src/Components/SetPomodoro/index.js
+++ src/Components/SetPomodoro/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
 const SetPomodoro = () => {
+
+  // Formulário de atualização do timer
+
   return (
-    <div></div>
+    <div class="form-container">
+      <form noValidate onSubmit={handleSubmit}>
+        <div className="input-wrapper">
+          <input type="number" name="work" className="input" onChange={handleChange} value={} />
+          <input type="number" name="shortBreak" className="input" onChange={handleChange} value={} />
+          <input type="number" name="longBreak" className="input" onChange={handleChange} value={} />
+        </div>
+        <button type="submit">Set Timer</button>
+      </form>
+    </div>
   )
 }
 
