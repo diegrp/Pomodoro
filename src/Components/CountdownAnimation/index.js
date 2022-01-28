ce962df89e0869f5b7bc9cc0db8cef5ebb763345
Diego Rocha <dkxever@gmail.com>
2022-01-28 19:42:17 -0300

    Binding props to the CountdownCircleTimer component

--- src/Components/CountdownAnimation/index.js
+++ src/Components/CountdownAnimation/index.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import { CountdownCircleTimer } from 'react-countdown-circle-timer';
 
-const CountdownAnimation = ({ key, timer, animation, children }) => {
+const CountdownAnimation = ({ key, timer, animate, children }) => {
   
   // CountdownCircleTimer e vínculo de props aos atributos do componente externo da lib instalada
   
   return (
-    <CountdownCircleTimer>
+    <CountdownCircleTimer
+      key={key}
+      isPlaying={animate}
+      duration={timer * 60}
+      colors={[
+        ["#FE6E6B"],
+        ["#FE6E6B"],
+        ["#FE6E6B"]
+      ]}
+      colorsTime={[
+        [0.33],
+        [0.33],
+        [0.33]
+      ]}
+      strokeWidth={6}
+      size={222}
+      trailColor="#151932"
+      onComplete={() => {
 
+      }}
+    >
+      {children}
     </CountdownCircleTimer>
   )
 }
